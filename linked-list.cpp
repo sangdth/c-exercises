105994fecac8187252d8b4e737d56f7858e8664f
Sang Dang <sang.dang@digia.com>
2020-11-25 21:11:37 +0200

    practice pointers

--- linked-list.cpp
+++ linked-list.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 using namespace std;
 
-struct Block {
-    int data;                         // Initialized and point nowhere
-    struct Block *previous {nullptr}; // without {nullptr} it will point to anywhere!
-    struct Block *next {nullptr};     // which is bad because it could generate garbage.
+struct Node {
+    int data;                    // Initialized and point nowhere
+    struct Node *prev {nullptr}; // without {nullptr} it will point to anywhere!
+    struct Node *next {nullptr}; // which is bad because it could generate garbage.
 };
 
+struct Node* head = NULL;
+
+char menu();
+void insert();
+void remove();
+void show();
+
 int main() {
-    cout << "Hello, world!" << endl;
+    cout << "Hello, doubly linked list!" << endl;
     return 0;
 }
 
--- pointer.cpp
+++ pointer.cpp
@@ -5,13 +5,20 @@ int main() {
     int *mypointer {nullptr};
     int age {36};
 
-    cout << "Value of age: " << age << endl;
-    cout << "Size of age: " << sizeof(age) << endl;
-    cout << "Address of age: " << &age << endl;
+    /* cout << "Value of age: " << age << endl; */
+    int temp1 {100};
+    int temp2 {200};
+    int *temp1_ptr {&temp1};
+    int *temp2_ptr {&temp2};
 
-    cout << "Value of mypointer: " << mypointer << endl;
-    /* cout << "Size of mypointer: " << sizeof(*mypointer) << endl; */
-    cout << "Address of mypointer: " << &mypointer << endl;
+    cout << "temp1_ptr is: " << *temp1_ptr << endl;
+
+    *temp1_ptr = 110;
+    cout << "temp1 should be changed to 110: " << temp1 << endl;
+    cout << "temp1_ptr before assignment should be same 110: " << *temp1_ptr << endl;
+
+    temp1_ptr = &temp2;
+    cout << "temp1_ptr after assignment: " << *temp1_ptr << endl;
 
     return 0;
 }
